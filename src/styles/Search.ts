e29fc64f0c8bee4d333a6e80a9a2fa286606c4de
RochaMoran <rochamoran1@gmail.com>
2022-05-03 10:19:36 +0100

    ADD: Search view

---
+++ src/styles/Search.ts
@@ -0,0 +1,98 @@
+import { StyleSheet } from "react-native";
+import { colors } from "./global";
+
+export const styles = StyleSheet.create({
+   searchContainer: {
+    backgroundColor: colors.primary,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: "center",
+    padding: 5
+   },
+   searchButton: {
+       marginHorizontal: 20,
+    },
+    searchInput: {
+        color: "white",
+        width: "75%",
+   },
+   searchTab: {
+    backgroundColor: colors.primary,
+    paddingVertical: 10,
+
+   },
+   searchTabItem: {
+    marginHorizontal: 20,
+    padding: 5,
+    },
+    searchTabItemText: {
+        textTransform: "uppercase" ,
+        color: "white",
+        fontWeight: "bold"
+    },
+    searchItemSelected: {
+        borderBottomColor: "black",
+        borderBottomWidth: 2
+    },
+    searchItemTextSelected: {
+        color: "black"
+    },
+    siteResult: {
+        backgroundColor: "#e0e0e0",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 5,
+        borderRadius: 5,
+        marginHorizontal: 10,
+        marginVertical: 5
+    },
+    siteResultImage: {
+        width: "35%",
+        height: "100%",
+        borderRadius: 5,
+        resizeMode: "cover"
+    },
+    titleItemSite: {
+        fontWeight: "bold",
+        fontSize: 14,
+        color: "#262626"
+    },
+    siteItemText: {
+        fontStyle: "italic",
+        color: "#5e5e5e"
+    },
+    containerResults: {
+    },
+    containerBasicInfo: {
+
+    },
+    containerCovidInfo: {
+
+    },
+    containerStatusInfo: {
+
+    },
+    statusTime: {
+        flexDirection: "row"
+    },
+    covidItem: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    covidItemTex: {
+        marginLeft: 5
+    },
+    siteStatusOpen: {
+        color: colors.primary,
+    },
+    siteStatusClose: {
+        color: "red",
+    },
+    statusOptional: {
+        color: "#d6930d"
+    },
+    statusObligatory: {
+        color: "#1b0991"
+    }
+});
--- src/views/Search.tsx
+++ src/views/Search.tsx
@@ -1,9 +1,258 @@
-import { View, Text} from "react-native";
+import { View, Text, TextInput, TouchableOpacity, ScrollView, Image } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome'
+import IconFA5 from 'react-native-vector-icons/FontAwesome5'
+import IconF from 'react-native-vector-icons/Fontisto'
+import IconI from "react-native-vector-icons/Ionicons";
+import { styles } from "../styles/Search";
 
 export default function Search() {
   return (
     <View>
-        <Text>Search</Text>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity style={styles.searchButton}>
+          <Icon name="search" size={20} color="white" />
+        </TouchableOpacity>
+        <TextInput
+          placeholderTextColor="#FFF"
+          style={styles.searchInput}
+          placeholder="Buscar..."
+        />
+        <TouchableOpacity style={styles.searchButton}>
+          <Icon name="microphone" size={20} color="white" />
+        </TouchableOpacity>
+      </View>
+      <ScrollView horizontal={true} style={styles.searchTab}>
+        <TouchableOpacity
+          style={[styles.searchTabItem, styles.searchItemSelected]}
+        >
+          <Text
+            style={[styles.searchTabItemText, styles.searchItemTextSelected]}
+          >
+            Lo mas popular
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.searchTabItem}>
+          <Text style={styles.searchTabItemText}>Lo mas nuevo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.searchTabItem}>
+          <Text style={styles.searchTabItemText}>Lo mas barato</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.searchTabItem}>
+          <Text style={styles.searchTabItemText}>Lo mas caro</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.searchTabItem}>
+          <Text style={styles.searchTabItemText}>Lo mas cercano</Text>
+        </TouchableOpacity>
+      </ScrollView>
+      <ScrollView style={styles.containerResults}>
+        <View style={styles.siteResult}>
+          <Image
+            style={styles.siteResultImage}
+            source={{
+              uri: "https://as01.epimg.net/diarioas/imagenes/2022/03/25/actualidad/1648212028_088443_1648212178_noticia_normal.jpg",
+            }}
+          />
+          <View>
+            <View style={styles.containerBasicInfo}>
+              <Text style={styles.titleItemSite}>MachuPicchu</Text>
+              <Text style={styles.siteItemText}>Peru</Text>
+            </View>
+            <View style={styles.containerCovidInfo}>
+              <Text style={styles.titleItemSite}>Covid Medidas</Text>
+              <View style={styles.covidItem}>
+                <IconFA5 name="head-side-mask" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText, styles.statusObligatory]}>Obligatorio</Text>
+              </View>
+              <View style={styles.covidItem}>
+                <IconF name="injection-syringe" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText, styles.statusOptional]}>Opcional</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.containerStatusInfo}>
+            <Text style={styles.titleItemSite}>Estado Apertura</Text>
+            <Text style={[styles.siteItemText, styles.siteStatusOpen]}>Abierto</Text>
+            <Text style={styles.titleItemSite}>Horarios:</Text>
+            <Text style={styles.siteItemText}>Abre: 7:00AM</Text>
+            <Text style={styles.siteItemText}>Cierra: 12:00PM</Text>
+          </View>
+        </View>
+        <View style={styles.siteResult}>
+          <Image
+            style={styles.siteResultImage}
+            source={{
+              uri: "https://as01.epimg.net/diarioas/imagenes/2022/03/25/actualidad/1648212028_088443_1648212178_noticia_normal.jpg",
+            }}
+          />
+          <View>
+            <View style={styles.containerBasicInfo}>
+              <Text style={styles.titleItemSite}>MachuPicchu</Text>
+              <Text style={styles.siteItemText}>Peru</Text>
+            </View>
+            <View style={styles.containerCovidInfo}>
+              <Text style={styles.titleItemSite}>Covid Medidas</Text>
+              <View style={styles.covidItem}>
+                <IconFA5 name="head-side-mask" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText, styles.statusObligatory]}>Obligatorio</Text>
+              </View>
+              <View style={styles.covidItem}>
+                <IconF name="injection-syringe" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText, styles.statusObligatory]}>Obligatorio</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.containerStatusInfo}>
+            <Text style={styles.titleItemSite}>Estado Apertura</Text>
+            <Text style={[styles.siteItemText, styles.siteStatusOpen]}>Abierto</Text>
+            <Text style={styles.titleItemSite}>Horarios:</Text>
+            <Text style={styles.siteItemText}>Abre: 7:00AM</Text>
+            <Text style={styles.siteItemText}>Cierra: 12:00PM</Text>
+          </View>
+        </View>
+        <View style={styles.siteResult}>
+          <Image
+            style={styles.siteResultImage}
+            source={{
+              uri: "https://as01.epimg.net/diarioas/imagenes/2022/03/25/actualidad/1648212028_088443_1648212178_noticia_normal.jpg",
+            }}
+          />
+          <View>
+            <View style={styles.containerBasicInfo}>
+              <Text style={styles.titleItemSite}>MachuPicchu</Text>
+              <Text style={styles.siteItemText}>Peru</Text>
+            </View>
+            <View style={styles.containerCovidInfo}>
+              <Text style={styles.titleItemSite}>Covid Medidas</Text>
+              <View style={styles.covidItem}>
+                <IconFA5 name="head-side-mask" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText, styles.statusObligatory]}>Obligatorio</Text>
+              </View>
+              <View style={styles.covidItem}>
+                <IconF name="injection-syringe" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText, styles.statusOptional]}>Opcional</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.containerStatusInfo}>
+            <Text style={styles.titleItemSite}>Estado Apertura</Text>
+            <Text style={[styles.siteItemText, styles.siteStatusClose]}>Cerrado</Text>
+            <Text style={styles.titleItemSite}>Horarios:</Text>
+            <Text style={styles.siteItemText}>Abre: 7:00AM</Text>
+            <Text style={styles.siteItemText}>Cierra: 12:00PM</Text>
+          </View>
+        </View>
+        <View style={styles.siteResult}>
+          <Image
+            style={styles.siteResultImage}
+            source={{
+              uri: "https://as01.epimg.net/diarioas/imagenes/2022/03/25/actualidad/1648212028_088443_1648212178_noticia_normal.jpg",
+            }}
+          />
+          <View>
+            <View style={styles.containerBasicInfo}>
+              <Text style={styles.titleItemSite}>MachuPicchu</Text>
+              <Text style={styles.siteItemText}>Peru</Text>
+            </View>
+            <View style={styles.containerCovidInfo}>
+              <Text style={styles.titleItemSite}>Covid Medidas</Text>
+              <View style={styles.covidItem}>
+                <IconFA5 name="head-side-mask" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText]}>Obligatorio</Text>
+              </View>
+              <View style={styles.covidItem}>
+                <IconF name="injection-syringe" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText]}>Obligatorio</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.containerStatusInfo}>
+            <Text style={styles.titleItemSite}>Estado Apertura</Text>
+            <Text style={[styles.siteItemText, styles.siteStatusOpen]}>Abierto</Text>
+            <Text style={styles.titleItemSite}>Horarios:</Text>
+            <Text style={styles.siteItemText}>Abre: 7:00AM</Text>
+            <Text style={styles.siteItemText}>Cierra: 12:00PM</Text>
+          </View>
+        </View>
+        <View style={styles.siteResult}>
+          <Image
+            style={styles.siteResultImage}
+            source={{
+              uri: "https://as01.epimg.net/diarioas/imagenes/2022/03/25/actualidad/1648212028_088443_1648212178_noticia_normal.jpg",
+            }}
+          />
+          <View>
+            <View style={styles.containerBasicInfo}>
+              <Text style={styles.titleItemSite}>MachuPicchu</Text>
+              <Text style={styles.siteItemText}>Peru</Text>
+            </View>
+            <View style={styles.containerCovidInfo}>
+              <Text style={styles.titleItemSite}>Covid Medidas</Text>
+              <View style={styles.covidItem}>
+                <IconFA5 name="head-side-mask" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText]}>Obligatorio</Text>
+              </View>
+              <View style={styles.covidItem}>
+                <IconF name="injection-syringe" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText]}>Obligatorio</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.containerStatusInfo}>
+            <Text style={styles.titleItemSite}>Estado Apertura</Text>
+            <Text style={[styles.siteItemText, styles.siteStatusOpen]}>Abierto</Text>
+            <Text style={styles.titleItemSite}>Horarios:</Text>
+            <Text style={styles.siteItemText}>Abre: 7:00AM</Text>
+            <Text style={styles.siteItemText}>Cierra: 12:00PM</Text>
+          </View>
+        </View>
+        <View style={styles.siteResult}>
+          <Image
+            style={styles.siteResultImage}
+            source={{
+              uri: "https://as01.epimg.net/diarioas/imagenes/2022/03/25/actualidad/1648212028_088443_1648212178_noticia_normal.jpg",
+            }}
+          />
+          <View style={styles.containerStatusInfo}>
+            <Text style={styles.titleItemSite}>Estado Apertura</Text>
+            <Text style={[styles.siteItemText, styles.siteStatusOpen]}>Abierto</Text>
+            <Text style={styles.titleItemSite}>Horarios:</Text>
+            <Text style={styles.siteItemText}>Abre: 7:00AM</Text>
+            <Text style={styles.siteItemText}>Cierra: 12:00PM</Text>
+          </View>
+        </View>
+        <View style={styles.siteResult}>
+          <Image
+            style={styles.siteResultImage}
+            source={{
+              uri: "https://as01.epimg.net/diarioas/imagenes/2022/03/25/actualidad/1648212028_088443_1648212178_noticia_normal.jpg",
+            }}
+          />
+          <View>
+            <View style={styles.containerBasicInfo}>
+              <Text style={styles.titleItemSite}>MachuPicchu</Text>
+              <Text style={styles.siteItemText}>Peru</Text>
+            </View>
+            <View style={styles.containerCovidInfo}>
+              <Text style={styles.titleItemSite}>Covid Medidas</Text>
+              <View style={styles.covidItem}>
+                <IconFA5 name="head-side-mask" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText, styles.statusObligatory]}>Obligatorio</Text>
+              </View>
+              <View style={styles.covidItem}>
+                <IconF name="injection-syringe" color="#5e5e5e" size={15} />
+                <Text style={[styles.covidItemTex, styles.siteItemText]}>Obligatorio</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.containerStatusInfo}>
+            <Text style={styles.titleItemSite}>Estado Apertura</Text>
+            <Text style={[styles.siteItemText, styles.siteStatusOpen]}>Abierto</Text>
+            <Text style={styles.titleItemSite}>Horarios:</Text>
+            <Text style={styles.siteItemText}>Abre: 7:00AM</Text>
+            <Text style={styles.siteItemText}>Cierra: 12:00PM</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
